7b2c3cd786097c304909a3b74b427c7562184d7f
OCEANO22 <n.oceano22@gmail.com>
2025-04-15 13:08:29 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { getUsers } from "./services/api";
+
+const users = ref([]);
+
+onMounted(async () => {
+  users.value = await getUsers();
+});
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="blogger in users.results" :key="blogger.id">{{ blogger.judul }}</li>
+    </ul>
+  </div>
+</template>
